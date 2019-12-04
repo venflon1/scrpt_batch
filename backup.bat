168b0894f14f4625a8985af03fe056ed309c7dfc
venflon1 <roberto.amato.11.93@gmail.com>
2019-12-04 09:28:42 +0000

    added references in file backup.bat

--- backup.bat
+++ backup.bat
@@ -2,4 +2,6 @@
 ::
 :: ho costruito un file bacth che esegue il backup di uno script sql prendendo da linea di comando alcuni  parametri come:
 :: %1 = user, %2 password (user e password per l'utenza di mysql), %3=script da fare backup, %4=file di output dove viene fatto il backup
+
+:: reference: https://www.liquidweb.com/kb/how-to-back-up-mysql-databases-from-the-command-line/
 mysqldump --user=%1 --password=%2 %3 > %4
